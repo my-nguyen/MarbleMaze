81431d07b0758b35fe7c21650ac879545c0ee1eb
My Nguyen <My@mys-macbook-pro.local>
2016-08-17 16:43:37 -0700

    Tilt to move: CMMotionManager

--- MarbleMaze/GameScene.swift
+++ MarbleMaze/GameScene.swift
@@ -7,6 +7,7 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 enum CollisionTypes: UInt32 {
     case Player = 1
@@ -17,6 +18,11 @@ enum CollisionTypes: UInt32 {
 }
 
 class GameScene: SKScene {
+
+    var player: SKSpriteNode!
+    var lastTouchPosition: CGPoint?
+    var motionManager: CMMotionManager!
+    
     override func didMoveToView(view: SKView) {
         // load a background picture
         let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -27,13 +33,64 @@ class GameScene: SKScene {
 
         // load data from file
         loadLevel()
+        // create a player
+        createPlayer()
+
+        // disable earth-like gravity
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+
+        // create a CMMotionManager, which detects motion
+        motionManager = CMMotionManager()
+        // start collecting motion information
+        motionManager.startAccelerometerUpdates()
     }
-    
+
+    /// hack to simulate moving the ball (player) using touch on the simulator
+    // the methods touchesBegan and touchesMoved set the lastTouchPosition property
+    // the methods touchesEnded and touchesCancelled unset the lastTouchPosition property
+    // the update method calculates the difference between the touch position and the player's position
+    //     and sets the world's gravity
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.locationInNode(self)
+            lastTouchPosition = location
+        }
     }
-   
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.locationInNode(self)
+            lastTouchPosition = location
+        }
+    }
+
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+#if (arch(i386) || arch(x86_64))
+        /// on an Intel CPU (simulator)
+        // unwrap the optional lastTouchPosition
+        if let currentTouch = lastTouchPosition {
+            // calculate the difference between the current touch and the player's position
+            let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
+            // use that diff to change the gravity value of the physics world
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
+#else
+        /// on an iOS device (phone or tablet)
+        // unwrap the optional accelerometer data
+        if let accelerometerData = motionManager.accelerometerData {
+            // change the gravity to reflect the accelerometer data
+            // the accelerometer Y is passed to CGVector's X, because the device is rotated to landscape
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
+        }
+#endif
     }
 
     func loadLevel() {
@@ -108,5 +165,21 @@ class GameScene: SKScene {
                 }
             }
         }
+    }
+
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        // player can't rotate
+        player.physicsBody!.allowsRotation = false
+        // give a lot of friction to the player's movement
+        player.physicsBody!.linearDamping = 0.5
+
+        player.physicsBody!.categoryBitMask = CollisionTypes.Player.rawValue
+        // get notified when player collides with star, vortex, or finish
+        player.physicsBody!.contactTestBitMask = CollisionTypes.Star.rawValue | CollisionTypes.Vortex.rawValue | CollisionTypes.Finish.rawValue
+        player.physicsBody!.collisionBitMask = CollisionTypes.Wall.rawValue
+        addChild(player)
     }
 }
